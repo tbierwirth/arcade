c240dd59ef388053eb7c635913316b5ebb4d2081
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-09-26 23:15:45 -0600

    Add show action

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -10,7 +10,23 @@ router.get("/", function(req, res, next) {
     })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
-      res.status(500).send({error})
+      res.status(500).send({ error })
+    });
+});
+
+router.get("/:id", function(req, res, next) {
+  Game.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error })
     });
 });
 
